b64f57db13def3b5ab9b68bb0d1fd76731bb6158
Tomas Vana <tomasvana@yahoo.com>
2025-11-07 19:45:39 +1100

    add my custom fish prompt for omf trout theme

---
+++ fish/fish_prompt_trout.sh
@@ -0,0 +1,63 @@
+# name: trout
+
+function _git_branch_name
+  echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
+end
+
+function _is_git_dirty
+  echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
+end
+
+function _file_count
+  ls -1 | wc -l | sed 's/\ //g'
+end
+
+function fish_prompt
+  set -l last_status $status
+  set -l yellow (set_color yellow)
+  set -l red (set_color red)
+  set -l green (set_color green)
+  set -l magenta (set_color magenta)
+  set -l normal (set_color normal)
+
+  if set -q VIRTUAL_ENV
+    echo -n (set_color -o blue)"("(basename $VIRTUAL_ENV)")"(set_color -o normal)
+  end
+
+  # Show current user in green parentheses
+  set user_info "$green($USER)$normal"
+
+  if [ (_git_branch_name) ]
+    set -l git_branch $magenta(_git_branch_name)
+    set git_info "$git_branch"
+
+    if [ (_is_git_dirty) ]
+      set git_info "$git_info$yellow*"
+    end
+
+    set git_info "$magenta($git_info$magenta)$normal"
+  end
+
+  if test $last_status = 0
+    set prompt " $green☆$normal "
+  else
+    set prompt " $red☆$normal "
+  end
+
+  echo -n -s $user_info $git_info $prompt
+end
+
+
+function fish_right_prompt
+  set -l blue (set_color blue)
+  set -l normal (set_color normal)
+
+  set -l path (pwd)
+  set -l count (_file_count)
+  set -l directory_info "($path ($count))"
+
+  set -l time (date '+%H:%M')
+  set -l time_info "$blue($time)$normal"
+
+  echo -n -s $directory_info $time_info
+end
